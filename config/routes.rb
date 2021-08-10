1b256adc3f24096a8ced1e982f17510e5da3da37
Benedikt Nolte <bene_nolte@me.com>
2021-08-10 16:00:29 +0200

    Done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/articles', to: 'articles#index'
+  # New Task
+  get '/articles/new', to: 'articles#new'
+  post '/articles', to: 'articles#create'
+  # Details
+  get '/articles/:id', to: 'articles#show', as: :article
+  # Edit
+  get "/articles/:id/edit", to: 'articles#edit', as: :article_edit
+  patch "/articles/:id", to: 'articles#update'
+  # Delete
+  delete '/articles/:id', to: 'articles#destroy'
+
+  # resources :articles
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# TODO: Write a seed to insert 100 posts in the database
+# Faker::Post.unique.name
+
+# puts 'Creating 10 fake posts...'
+10.times do
+  post = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Twitter.status(include_user: false)[:text],
+  )
+  post.save!
+end
+# puts 'Finished!'
